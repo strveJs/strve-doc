cb62c10acee00ec2a7f28818456dfe4e9c598810
maomincoding <17864296568@163.com>
2023-04-13 13:36:24 +0800

    init

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -12,12 +12,6 @@ export default {
 			titleTemplate: 'A JS library that can convert strings into view',
 			description: 'A JS library that can convert strings into view',
 		},
-		'/zh/': {
-			lang: 'zh-CN',
-			title: 'Strve.js',
-			titleTemplate: '一个可以将字符串转换为视图的JS库',
-			description: '一个可以将字符串转换为视图的JS库',
-		},
 	},
 	themeConfig: {
 		logo: '/logo.png',
@@ -44,8 +38,8 @@ export default {
 					{
 						text: 'Languages',
 						items: [
-							{ text: 'English', link: '/' },
-							{ text: '简体中文', link: '/zh/' },
+							{ text: 'English', 	link: 'https://maomincoding.github.io/strve-doc/'},
+							{ text: '简体中文', link: 'https://maomincoding.github.io/strve-doc-zh/'},
 						],
 					},
 				],
@@ -135,114 +129,7 @@ export default {
 						],
 					},
 				],
-			},
-			'/zh/': {
-				nav: [
-					{
-						text: '演练场',
-						link: '/zh/playground/',
-					},
-					{
-						text: '更新日志',
-						link: '/zh/changeLog/',
-					},
-					{
-						text: '选择语言',
-						items: [
-							{ text: 'English', link: '/' },
-							{ text: '简体中文', link: '/zh/' },
-						],
-					},
-				],
-				outlineTitle: '此页',
-				sidebar: [
-					{
-						text: '指导',
-						collapsible: true,
-						items: [
-							{
-								text: '介绍',
-								link: '/zh/guide/introduce/',
-							},
-							{
-								text: '开始',
-								link: '/zh/guide/started/',
-							},
-							{
-								text: '安装',
-								link: '/zh/guide/install/',
-							},
-						],
-					},
-					{
-						text: '要点',
-						collapsible: true,
-						items: [
-							{
-								text: 'API',
-								link: '/zh/essentials/api/',
-							},
-							{
-								text: '用法',
-								link: '/zh/essentials/usage/',
-							},
-						],
-					},
-					{
-						text: '工具',
-						collapsible: true,
-						items: [
-							{
-								text: 'CreateStrveApp',
-								link: '/zh/tool/createStrveApp/',
-							},
-							{
-								text: 'CreateStrve',
-								link: '/zh/tool/createStrve/',
-							},
-							{
-								text: 'StrveRouter',
-								link: '/zh/tool/strveRouter/',
-							},
-							{
-								text: 'BabelPluginStrve',
-								link: '/zh/tool/babelPluginStrve/',
-							},
-
-						],
-					},
-					{
-						text: '其他',
-						collapsible: true,
-						items: [
-							{
-								text: 'SFC',
-								link: '/zh/other/sfc/',
-							},
-							{
-								text: '适配',
-								link: '/zh/other/adapt/',
-							},
-							{
-								text: 'IDE 支持',
-								link: '/zh/other/ide/',
-							},
-							{
-								text: 'UI 框架',
-								link: '/zh/other/ui/',
-							},
-							{
-								text: '浏览器兼容性',
-								link: '/zh/other/browser/',
-							},
-							{
-								text: '关于',
-								link: '/zh/other/about/',
-							},
-						],
-					},
-				],
-			},
+			}
 		},
 		footer: {
 			message: 'Released under the MIT License.',
