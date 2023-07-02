bd61ee49204c20bdc4349058e1c1be481787263c
maomincoding <17864296568@163.com>
2023-07-03 01:03:23 +0800

    v5.6.0

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:maomincoding/strve-doc.git master:pages
+git push -f git@github.com:maomincoding/strve-doc.git main:pages
 
 cd -
